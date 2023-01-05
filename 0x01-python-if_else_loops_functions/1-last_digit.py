17f8261fa870e4bbd706e191668aca8397a6bde7
lil-armstrong <oebong1@gmail.com>
2023-01-05 05:15:05 +0100

    refactor

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
-report = "is 0"
+report = ""
 sym = '-' if number < 0 else ""
 last_digit = sym + last_digit
 
+if int(last_digit == 0):
+    report = "is 0"
 if int(last_digit) < 6:
     report = "is less than 6 and not 0"
 elif int(last_digit) > 5:
