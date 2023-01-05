6a1b123a10df5401ab8013e4952a2a154666a357
lil-armstrong <oebong1@gmail.com>
2023-01-05 04:50:17 +0100

    refactor

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if j == 9 and i == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+"""Program prints numbers from 0 to 99 using only one loop."""
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
