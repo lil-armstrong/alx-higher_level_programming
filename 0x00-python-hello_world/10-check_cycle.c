2be79febdd606c2e0ada77ce59370bc4072bcbe9
lil-armstrong <oebong1@gmail.com>
2023-01-05 05:43:30 +0100

    add C task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,7 +5,24 @@
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
- int check_circle (listint * list)
- {
+int check_circle(listint_t *list)
+{
+	listint_t *slow, *fast;
 
- }
+	if (!list)
+	{
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,10 +1,9 @@
 #ifndef LISTS_H
 #define LIST_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-
 
 /**
  * struct listint_s - singly linked list
@@ -13,14 +12,15 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct listint_s {
+typedef struct listint_s
+{
 	int n;
-	struct listint_s next
+	struct listint_s *next;
 } listint_t;
 
-listint_t* add_nodeint(listint_t**, const int);
-size_t print_listint(listint *);
-int check_cycle(listint *);
-void free_listint(listint *);
+listint_t *add_nodeint(listint_t **, const int);
+size_t print_listint(listint_t *h);
+int check_cycle(listint_t *);
+void free_listint(listint_t *);
 
 #endif /* LISTS_H */
