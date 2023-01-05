ec88f7edcbc3fa00d9c88a416c7ba010115bf27f
lil-armstrong <oebong1@gmail.com>
2023-01-05 05:57:17 +0100

    add adv tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    """Create a copy of a string, removing the character at the position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
