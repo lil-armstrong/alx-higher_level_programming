3fde16709f0ba6d45cd900766db672625962b40d
lil-armstrong <oebong1@gmail.com>
2023-01-05 05:13:45 +0100

    refactor

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,11 @@ number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
 report = "is 0"
 sym = '-' if number < 0 else ""
-if int(last_digit) > 0:
-    if int(last_digit) < 6:
-        report = "is less than 6 and not 0"
-    elif int(last_digit) > 5:
-        report = "is greater than 5"
 last_digit = sym + last_digit
+
+if int(last_digit) < 6:
+    report = "is less than 6 and not 0"
+elif int(last_digit) > 5:
+    report = "is greater than 5"
 
 print(f"Last digit of {number} is {last_digit} and {report}")
