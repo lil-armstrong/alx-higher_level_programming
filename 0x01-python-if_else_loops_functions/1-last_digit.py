d022265332ba9b3f0ab14c8b943749668c188954
lil-armstrong <oebong1@gmail.com>
2023-01-05 05:17:19 +0100

    refactor

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ last_digit = sym + last_digit
 print(last_digit)
 if int(last_digit == 0):
     report = "is 0"
-if int(last_digit) < 6:
+elif int(last_digit) < 6:
     report = "is less than 6 and not 0"
 elif int(last_digit) > 5:
     report = "is greater than 5"
