30b9519aeae727e5e4862f4504487a4807414e1e
lil-armstrong <oebong1@gmail.com>
2023-01-06 04:24:06 +0100

    Add project 3 and task 0

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function.
+
+    Args:
+        a: first integer
+        b:  second integer
+    ______________________
+
+    Return:
+        The return value. a + b
+    _____________________
+
+    """
+    return (a + b)
