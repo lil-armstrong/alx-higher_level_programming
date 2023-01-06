282bba8e877a1bca6e2e68ab250f3c1a1d08d884
lil-armstrong <oebong1@gmail.com>
2023-01-06 05:23:56 +0100

    add task 4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """Print the result of the addition of all arguments."""
+    import sys
+
+    ac = sys.argv[1:]
+    sum = 0
+    for i in ac:
+        if i.isnumeric() or int(i):
+            sum += int(i)
+    print("{:d}".format(sum))
