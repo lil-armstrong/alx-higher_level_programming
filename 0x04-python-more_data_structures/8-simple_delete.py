da70cd1938708c908a2a85fd4d38251a8da47e09
lil-armstrong <oebong1@gmail.com>
2023-01-14 05:59:04 +0100

    add 0x04(8)

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    """Delete a key in a dictionary."""
+    if key in a_dictionary:
+        #  Delete key
+        del a_dictionary[key]
+
+    return a_dictionary
