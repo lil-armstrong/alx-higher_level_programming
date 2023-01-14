3d3a4f3bac91fcf84f8fa1e4a399976060b55d87
lil-armstrong <oebong1@gmail.com>
2023-01-14 06:08:01 +0100

    add 0x04(10)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """Return a key with the biggest interger value."""
+    score = None
+    keys = list(a_dictionary.keys())
+    scores = list(a_dictionary.values())
+    score = keys[scores.index(max(scores))]
+    return score
