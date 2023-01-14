5f89d1254ba71bf84b64d91ca0fe19a7a4ab54e6
lil-armstrong <oebong1@gmail.com>
2023-01-14 05:40:09 +0100

    chore: setup
    - add task 0 - 5

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """Compute the square value of all integers of a matrix."""
+    new_matrix = []
+
+    for row in matrix:
+        new_matrix.append(list(map(lambda x: x**2, row)))
+
+    return new_matrix
