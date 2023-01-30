e277716711a2b9991da4cc32f00344160ba62320
lil-armstrong <oebong1@gmail.com>
2023-01-30 11:25:24 +0100

    add task (1)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class: Square."""
+
+
+class Square:
+    """Represent a square class."""
+
+    def __init__(self, size=None):
+        """Class initializer."""
+        self.__size = size
