9734b801c8ce22fccf8d1308270262f1c89a7577
lil-armstrong <oebong1@gmail.com>
2023-01-30 17:36:57 +0100

    add test case 0x07(0)

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Add two(2) integers.
+
+    Args:
+    a: integer or float
+    b: integer or float
+
+    """
+    if (not isinstance(a, (int, float))):
+        raise TypeError("a must be a integer")
+
+    if (not isinstance(b, (int, float))):
+        raise TypeError("b must be a integer")
+
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
+
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,4 +1,12 @@
+
 ## Tasks
+---
+
+- All your tests should be executed by using this command: `python3 -m doctest ./tests/*`
+
+- All your modules should have a documentation `(python3 -c 'print(__import__("my_module").__doc__)')`
+
+- All your functions should have a documentation (python3 -c 'print(__import__("my_module").my_function.__doc__)')
 
 0.  Write a function that adds 2 integers.
 
