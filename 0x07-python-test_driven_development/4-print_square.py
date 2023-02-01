311870416b88252b7b6047afd4150cd0318af578
lil-armstrong <oebong1@gmail.com>
2023-02-01 09:28:35 +0100

    add task with test cases 0x07(4)

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def print_square(size):
+    """
+    Print a square with the character #.
+
+    Args:
+    size: the size length of the square.
+    """
+    if (not isinstance(size, (int))):
+        raise TypeError("size must be an integer")
+    elif (size <= 0):
+        raise TypeError("size must be >= 0")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print("")
