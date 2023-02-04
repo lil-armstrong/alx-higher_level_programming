d305618c12271466a24b6174f091ae8c5c2ba65d
lil-armstrong <oebong1@gmail.com>
2023-02-04 07:38:50 +0100

    refactor task 4 (0x07)

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Print a square with the character #."""
+
 
 def print_square(size):
     """Print a square with the character #.
 
     Args:
     size: the size length of the square.
+
+    Raises:
+    TypeError: if size is not an integer or is zero(0)
     """
     if (not isinstance(size, (int))):
         raise TypeError("size must be an integer")
