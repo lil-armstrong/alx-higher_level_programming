2f398cd8f420254ec8aca42421501c0ce16af147
lil-armstrong <oebong1@gmail.com>
2023-02-04 07:25:44 +0100

    refactor task 0 (0x07)

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    """Add two(2) integers.
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed
 
     Args:
     a: integer or float
     b: integer or float
 
+    Raises:
+    TypeError: if either a or b is non-integer or non-float
     """
     if (not isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
@@ -14,7 +18,4 @@ def add_integer(a, b=98):
     if (not isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-
-    return (a + b)
+    return (int(a) + int(b))
