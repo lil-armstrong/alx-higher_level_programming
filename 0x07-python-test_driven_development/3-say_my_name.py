16442860fba2d06929835571ff6e6bbbd7486d13
lil-armstrong <oebong1@gmail.com>
2023-02-04 07:34:30 +0100

    refactor task 2 (0x07)

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Print a string in the format ``My name is <first name> <last name>.``"""
+
 
 def say_my_name(first_name, last_name=""):
-    """Print My name is <first name> <last name>."""
+    """Print My name is <first name> <last name>.
+
+    Args:
+    first_name: string
+    last_name: string. Default is empty string
+
+    Raises:
+    TypeError: if either first_name and last_name is not a string
+    """
     if (not isinstance(first_name, (str))):
         raise TypeError("first_name must be a string")
 
