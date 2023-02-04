8e27eb91b617fd16f5530c71e78a60304ae25d0f
lil-armstrong <oebong1@gmail.com>
2023-02-04 07:21:14 +0100

    refactor task 0 (0x07)

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,10 +9,10 @@ def add_integer(a, b=98):
 
     """
     if (not isinstance(a, (int, float))):
-        raise TypeError("a must be a integer")
+        raise TypeError("a must be an integer")
 
     if (not isinstance(b, (int, float))):
-        raise TypeError("b must be a integer")
+        raise TypeError("b must be an integer")
 
     a = int(a)
     b = int(b)
