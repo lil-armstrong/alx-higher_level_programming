8a3bd901bb88ad651a33353995db80031d6f95ec
lil-armstrong <oebong1@gmail.com>
2023-02-04 08:01:04 +0100

    refactor task 4 (0x07)

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,14 +9,22 @@ def print_square(size):
     size: the size length of the square.
 
     Raises:
-    TypeError: if size is not an integer or is zero(0)
+    TypeError: if size is not an integer
+    ValueError: if size is zero(0)
+    TypeError: if size is float and size < 0
     """
+    if (type(size) in [float]):
+        if (size < 0):
+            raise TypeError("size must be an integer")
+        size = int(size)
+
     if (not isinstance(size, (int))):
         raise TypeError("size must be an integer")
-    elif (size <= 0):
+
+    if (size < 0):
         raise ValueError("size must be >= 0")
-    else:
-        for i in range(size):
-            for j in range(size):
-                print("#", end="")
-            print("")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
