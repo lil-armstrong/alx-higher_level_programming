98991221c63f4ddf2251ce57f7e2949ee4415aad
lil-armstrong <oebong1@gmail.com>
2023-02-08 19:32:40 +0100

    refactor - add size validation logic

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,6 +10,7 @@ class Square (Rectangle):
     def __init__(self, size):
         """Initialize Square object"""
         super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
