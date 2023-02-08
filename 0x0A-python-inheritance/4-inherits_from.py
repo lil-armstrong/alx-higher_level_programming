ca2b5a50ab0c98b008e98817e5662bf7506bdad4
lil-armstrong <oebong1@gmail.com>
2023-02-08 19:52:44 +0100

    refactor

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,7 +15,5 @@ def inherits_from(obj, a_class):
     True: if object is an instance
     False: otherwise
     """
-    if isinstance(obj, a_class) and not type(obj) is a_class:
-        return True
-    else:
-        False
+    return issubclass(obj.
+                      __class__, a_class) and type(obj) != a_class
