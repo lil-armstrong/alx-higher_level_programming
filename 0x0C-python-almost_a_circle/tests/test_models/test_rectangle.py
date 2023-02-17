f3c5bf248d46a9a1a1ebec75985323210f68474f
lil-armstrong <oebong1@gmail.com>
2023-02-17 23:03:34 +0100

    add test for rectangle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Define unittest for models.rectangle"""
+
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
