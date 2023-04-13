5da47c5ecb319d3435c04f4830e11b6717191527
lil-armstrong <oebong1@gmail.com>
2023-04-14 00:46:20 +0100

    chore(0x12): add all task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const {argv} = require('node:process');
+const { argv } = require('node:process');
 const arg1 = +argv[2];
-function factorial (n){
-	if(Number.isNaN(n) || n <= 1)
-		return 1
-	return n * factorial(n-1);
+function factorial (n) {
+  if (Number.isNaN(n) || n <= 1) { return 1; }
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(arg1));
-
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const {argv} = require('node:process');
-const args = argv.slice(2)
-if(!args.length || args.length === 1)
-	console.log(0);
-else{
-	const sorted = args.sort();
-	console.log(sorted);
-	console.log(sorted[sorted.length - 2]);
+const { argv } = require('node:process');
+const args = argv.slice(2);
+if (!args.length || args.length === 1) { console.log(0); } else {
+  const sorted = args.sort();
+  console.log(sorted);
+  console.log(sorted[sorted.length - 2]);
 }
