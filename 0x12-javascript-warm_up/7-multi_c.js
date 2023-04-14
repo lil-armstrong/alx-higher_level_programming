6feb84530f96384f87e9f4525fe8f4a8cfdb1911
lil-armstrong <oebong1@gmail,com>
2023-04-14 08:00:14 +0100

    refactor(0x12)

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const n = +argv[2];
-
-if (Number.isNaN(n) || n <= 0) { console.log('Missing number of occurrences'); } else {
-  for (let i = 0; i < n; i++) {
-    console.log('C is fun');
+if (n > 0) {
+  if (Number.isNaN(n)) { console.log('Missing number of occurrences'); } else {
+    for (let i = 0; i < n; i++) {
+      console.log('C is fun');
+    }
   }
 }
