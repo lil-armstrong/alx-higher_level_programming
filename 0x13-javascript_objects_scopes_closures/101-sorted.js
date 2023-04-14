8f94141d3e3948ad1c344a7b6128aab326f12427
lil-armstrong <oebong1@gmail,com>
2023-04-14 11:31:39 +0100

    refactor(0x13)

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-const {dict} = require('./101-data');
+const { dict } = require('./101-data');
 const obj = {};
-Object.entries(dict).forEach(([k, v]) =>{
-	if(v in obj)
-		obj[v].push(k);
-	else
-		obj[v]=[k];
+Object.entries(dict).forEach(([k, v]) => {
+  if (v in obj) { obj[v].push(k); } else { obj[v] = [k]; }
 });
 
 console.log(obj);
