90a28ff71011eef53c9b758fa9ce722482847c42
lil-armstrong <oebong1@gmail,com>
2023-04-14 11:29:06 +0100

    add(0x13) tasj 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const {dict} = require('./101-data');
+const obj = {};
+Object.entries(dict).forEach(([k, v]) =>{
+	if(v in obj)
+		obj[v].push(k);
+	else
+		obj[v]=[k];
+});
+
+console.log(obj);
