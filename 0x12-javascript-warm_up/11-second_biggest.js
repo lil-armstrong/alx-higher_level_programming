71420656cad7732af00cc6830e38b4ba7d026a96
lil-armstrong <oebong1@gmail,com>
2023-04-14 08:11:38 +0100

    refactor(0x12)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 const { argv } = require('process');
 const args = argv.slice(2);
 if (args.length <= 0 || args.length === 1) { console.log(0); } else {
-  const sorted = args.sort((a, b) => Number(a) > Number(b) ? 1 : -1 );
+  const sorted = args.sort((a, b) => Number(a) > Number(b) ? 1 : -1);
   console.log(sorted[sorted.length - 2]);
 }
