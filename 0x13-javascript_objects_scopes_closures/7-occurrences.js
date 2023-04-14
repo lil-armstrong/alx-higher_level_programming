cf5db63e49c76c674c7bd2c5c27dbfdb320186d2
lil-armstrong <oebong1@gmail,com>
2023-04-14 07:10:47 +0100

    add(0x13):task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement){
+	let count = 0;
+	if (list.length == 0) return 0;
+
+	list.forEach((i)=>{
+		if(i === searchElement) count++;
+	})
+	return count;
+}
