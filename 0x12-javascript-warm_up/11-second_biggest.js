d31571d82246257f7c466caba8cb0884e53ce224
lil-armstrong <oebong1@gmail,com>
2023-04-14 08:10:57 +0100

    refactor(0x12)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const args = argv.slice(2);
-if (!args.length || args.length === 1) { console.log(0); } else {
-  const sorted = args.sort();
+if (args.length <= 0 || args.length === 1) { console.log(0); } else {
+  const sorted = args.sort((a, b) => Number(a) > Number(b) ? 1 : -1 );
   console.log(sorted[sorted.length - 2]);
 }
