dc0228356493ae67988ff6ccf510205d24ab59ed
lil-armstrong <oebong1@gmail,com>
2023-04-14 07:50:11 +0100

    refactor(0x12)

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,5 +3,5 @@ const { argv } = require('node:process');
 if (argv.length > 2) {
   if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
 } else {
-  console.log('No arguments');
+  console.log('No argument');
 }
