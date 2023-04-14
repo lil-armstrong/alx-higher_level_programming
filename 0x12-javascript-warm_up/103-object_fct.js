4e0ea7f92014a985f4c669d6b9e05de52f80ec70
lil-armstrong <oebong1@gmail,com>
2023-04-14 08:42:57 +0100

    add(0x12):task 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,12 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function() {
-	this.value += 1
-}
+myObject.incr = function () {
+  this.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
