38d76954ee21a1be13d881b752cad6dd3006c55e
lil-armstrong <oebong1@gmail,com>
2023-04-14 06:22:55 +0100

    refactor(0x13)

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const {argv}=require('node:process');
 
 module.exports = class Rectangle {
-	constructor(w, h){
-		this.width = w;
-		this.height = h;
-	}
-}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
