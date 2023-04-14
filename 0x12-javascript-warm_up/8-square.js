a7758cf6677d24a8c63af6f45b4e5c4213b646bd
lil-armstrong <oebong1@gmail,com>
2023-04-14 08:02:01 +0100

    refactor(0x12)

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const n = +argv[2];
-
-if (Number.isNaN(n) || n <= 0) { console.error('Missing size'); } else {
-  for (let i = 0; i < n; ++i) {
-    let row = '';
-    for (let j = 0; j < n; ++j) {
-      row += 'X';
+if (n > 0) {
+  if (Number.isNaN(n)) { console.error('Missing size'); } else {
+    for (let i = 0; i < n; ++i) {
+      let row = '';
+      for (let j = 0; j < n; ++j) {
+        row += 'X';
+      }
+      console.log(row);
     }
-    console.log(row);
   }
 }
