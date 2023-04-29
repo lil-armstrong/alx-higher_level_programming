45bf52182005bd8a218ba6ce99e5a5f257dabbac
lil-armstrong <oebong1@gmail.com>
2023-04-29 14:25:56 +0100

    refactor

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,13 +7,13 @@ contained in the database hbtn_0e_101_usa
 import sys
 from relationship_state import Base, State
 from relationship_city import Base, City
-from sqlalchemy import (create_engine)
+from sqlalchemy import (create_engine, MetaData)
 from sqlalchemy.orm import (sessionmaker)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
         sys.argv[1], sys.argv[2], 3306, sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    metadata = MetaData(bind=engine, schema=Base.metadata)
     Session = sessionmaker(bind=engine)
     session = Session()
 
---
+++ 0x0F-python-object_relational_mapping/fake_relationship.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import (Column, Integer, String)
+
+Base = declarative_base()
+
+class State(Base):
+    """ This is the State Class and it inherts from BaseModel """
+    __tablename__ = "states"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = []
+
