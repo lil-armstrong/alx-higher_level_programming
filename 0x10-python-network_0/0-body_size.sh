6219c7ed1848d69561976ed063c42a1dcad65f38
lil-armstrong <oebong1@gmail,com>
2023-04-30 07:33:22 +0100

    refactor

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send request to URL and get size of response body
-RESPONSE=$(curl -sSLw '%{size_download}\n' -o /dev/null "$1")
-
-# Display size of response body in bytes
-echo "Size of response body: $RESPONSE bytes"
+SIZE=$(curl -s -o /dev/null -w "%{size_download}" "$1")
+echo "Size of the response body: $SIZE bytes"
