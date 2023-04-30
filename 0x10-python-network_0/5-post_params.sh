81aa9830472620205bc928d263906d1a5e83ba5e
lil-armstrong <oebong1@gmail,com>
2023-04-30 07:57:48 +0100

    refactor

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays the body of the response
-curl -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST $1
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
