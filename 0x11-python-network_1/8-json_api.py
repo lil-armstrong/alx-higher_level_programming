93ed15a943d7d14e7ad5149b6331b96340577c3b
lil-armstrong <oebong1@gmail.com>
2023-05-16 19:40:45 +0100

    refactor

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 
 """
-Script takes in a letter and sends a POST request \
-    to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+Script takes in a letter and sends a POST request 
+    to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
 
 if __name__ == '__main__':
     import requests
     import sys
 
+    class NoResultException(Exception):
+        pass
+
     args = sys.argv[1:]
 
-    if len(args) < 1:
-        value = ""
-    else:
+    if len(args):
         value = args[0]
+    else:
+        value = ""
 
     try:
         payload = {'q': value}
         url = 'http://0.0.0.0:5000/search_user'
 
-        r = requests.post(url, json=payload)
+        r = requests.post(url, data=payload)
         data = r.json()
 
-        print("[%s] %s" % (data.id, data.name))
-    except requests.exceptions.InvalidJSONError as e:
+        if len(data) < 1:
+            raise NoResultException("No result")
+        print("[%s] %s" % (data.get("id", ""), data.get("name", "")))
+    except ValueError as e:
         print("Not a valid JSON")
-    except requests.exceptions.JSONDecodeError as e:
-        print("No result")
+    except NoResultException as e:
+        print(str(e))
