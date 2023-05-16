e6bec03f4f00f09687814836b77f8409a7ef1413
lil-armstrong <oebong1@gmail.com>
2023-05-16 20:42:12 +0100

    add task 9 (0x11)

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -16,17 +16,18 @@ if __name__ == '__main__':
         print("Usage: %s <GITHUB USERNAME> <GITHUB PASSWORD>" % (sys.argv[0]))
 
     else:
-        try: 
+        try:
             username = args[0]
             secret = args[1]
-            url = 'https://api.github.com/authorizations'
+            url = "https://api.github.com/user"
             headers = {
-                'X-GitHub-Api-Version': '2022-11-28'
+                'X-GitHub-Api-Version': '2022-11-28',
+                "Accept": "application/vnd.github+json",
             }
             auth = (username, secret)
-            response = requests.post(url, headers=headers, auth=auth)
+            response = requests.get(url, headers=headers, auth=auth)
             data = response.json()
-            
+
             print(data.get("id", ""))
         except ValueError as e:
             print(e)
