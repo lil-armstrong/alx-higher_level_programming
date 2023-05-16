95e634e7e36b00e1970ca168fa57bef7062be1cd
lil-armstrong <oebong1@gmail.com>
2023-05-16 20:44:38 +0100

    refactor

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-Script takes in a letter and sends a POST request 
-    to http://0.0.0.0:5000/search_user with the letter as a parameter
+Script takes in a letter and sends a POST \
+    request to http://0.0.0.0:5000/search_user \
+        with the letter as a parameter
 """
 
 if __name__ == '__main__':
