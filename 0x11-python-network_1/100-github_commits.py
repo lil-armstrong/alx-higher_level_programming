28a35e7bb836cf857cd371f7720b4a61059c6277
lil-armstrong <oebong1@gmail.com>
2023-05-16 21:06:39 +0100

    refactor

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,19 +13,20 @@ if __name__ == "__main__":
     if len(args) < 2:
         print("Usage %s <GIT REPO NAME> <GIT OWNER NAME>")
     else:
-        repo = args[0]
-        owner = args[1]
-        url = " https://api.github.com/repos/%s/%s/commits" % (repo, owner)
-        headers = {
-            'X-GitHub-Api-Version': '2022-11-28',
-            "Accept": "application/vnd.github+json"
-        }
-        auth = ("lil-armstrong", "github_pat_11AHPGKRY03" +
-                "Etw2izLsNE6_BL9SBOiwyrIRXPebqSXDem2yH6wnTy5" +
-                "m0JygeuL4be4DAMKP64MPVp5Y104")
-        response = requests.get(url, headers=headers,
-                                auth=auth, params={'per_page': 10})
-        commits = response.json()
-        for commit in commits:
-            print('%s: %s' % (commit.get("sha", ""),
-                  commit["commit"]["author"]["name"]))
+        try:
+            repo = args[0]
+            owner = args[1]
+            url = " https://api.github.com/repos/%s/%s/commits" % (owner, repo)
+            headers = {
+                'X-GitHub-Api-Version': '2022-11-28',
+                "Accept": "application/vnd.github+json"
+            }
+
+            response = requests.get(url, headers=headers,
+                                    params={'per_page': 10})
+            commits = response.json()
+            for commit in commits:
+                print('%s: %s' % (commit.get("sha", ""),
+                                  commit["commit"]["author"]["name"]))
+        except ValueError as e:
+            print(e)
