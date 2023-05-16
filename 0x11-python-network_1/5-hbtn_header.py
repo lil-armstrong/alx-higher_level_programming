dfe6aed1c64f538ff0d33d15bdc8b56ba630caa9
lil-armstrong <oebong1@gmail.com>
2023-05-16 14:19:02 +0100

    add task 5 (0x11)

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,10 +9,10 @@ import sys
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-    if len(args) < 1:
-        print("Usage: %s <URL>" % sys.argv[0])
+    if (len(args) < 1):
+        print("Usage: %s <URL>" % (sys.argv[0]))
     else:
-        url = "https://alx-intranet.hbtn.io/status"
+        url = args[0]
         req = requests.get(url)
         headers = req.headers
         print("%s" % (headers.get("X-Request-Id", None)))
