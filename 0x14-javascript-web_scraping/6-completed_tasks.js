aed3f6d992f4b6c0fa7700fbd1b77178af6f2502
lil-armstrong <oebong1@gmail.com>
2023-05-24 22:44:11 +0100

    refactor

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,26 @@
 #!/usr/bin/node
-const req = require('request');
 
+const request = require("request");
 const url = process.argv[2];
 
-req.get(url, (err, response) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  if (response.statusCode === 200) {
-    const obj = JSON.parse(response?.body);
-    const completed = {};
-
-    obj.forEach((task) => {
-      if (task.completed) {
-        if (task.userId in completed) {
-          completed[task.userId]++;
-        } else {
-          completed[task.userId] = 1;
-        }
-      }
-    });
-
-    console.log(completed);
-    return;
-  }
-
-  console.error(`Error code: ${response.statusCode}`);
+request(url, function (err, response, body) {
+	if (err) {
+		console.log(err);
+	} else if (response.statusCode === 200) {
+		const completed = {};
+		const tasks = JSON.parse(body);
+		for (const i in tasks) {
+			const task = tasks[i];
+			if (task.completed === true) {
+				if (completed[task.userId] === undefined) {
+					completed[task.userId] = 1;
+				} else {
+					completed[task.userId]++;
+				}
+			}
+		}
+		console.log(completed);
+	} else {
+		console.log("An error occured. Status code: " + response.statusCode);
+	}
 });
