ad07a89011faf9480391062894db949bc87490cc
lil-armstrong <oebong1@gmail.com>
2023-05-24 22:37:33 +0100

    refactor

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
 /* Script gets the content of a webpage and stores it in a file */
-const req = require("request");
-const fs = require("fs");
+const req = require('request');
+const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
 req.get(url, (err, response, body) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-	if (response.statusCode === 200) {
-		fs.writeFile(filePath, body, "utf-8", (err) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-		});
-	}
+  if (response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });
