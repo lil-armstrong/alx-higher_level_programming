57cf0f9ef3f0ec706c7e8ee46ca4b80566c5002b
lil-armstrong <oebong1@gmail.com>
2023-05-24 21:03:08 +0100

    refactor

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf-8", (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-	console.log(data);
+  console.log(data);
 });
