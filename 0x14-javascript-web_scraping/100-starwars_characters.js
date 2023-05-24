da63f5be317648def3fed0b40a038e6cdb89eed0
lil-armstrong <oebong1@gmail.com>
2023-05-24 23:00:57 +0100

    add task 100 (0x14)

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,33 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
-const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
-
-request(API_URL + episodeNum, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    const characters = responseJSON.characters;
-    characters.forEach((link) => {
-      request.get(link, (err, resp) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        if (resp.statusCode === 200) {
-          const obj = JSON.parse(resp.body);
-          console.log(obj?.name);
-          return;
-        }
-        console.error(
-					`Encountered an error with status code: ${resp.statusCode}`
-        );
-      });
-    });
-  } else {
-    console.log('Error code: ' + response.statusCode);
-  }
+const req = require("request");
+const id = process.argv[2];
+const url = "https://swapi-api.hbtn.io/api/films/";
+req.get(url + id, function (error, res, body) {
+	if (error) {
+		console.log(error);
+	}
+	const data = JSON.parse(body);
+	const dd = data.characters;
+	for (const i of dd) {
+		req.get(i, function (error, res, body1) {
+			if (error) {
+				console.log(error);
+			}
+			const data1 = JSON.parse(body1);
+			console.log(data1.name);
+		});
+	}
 });
