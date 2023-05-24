5ce0b2d1e1f3373261ae19ad79e0373921c40ba2
lil-armstrong <oebong1@gmail.com>
2023-05-24 21:57:31 +0100

    refactor

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const req = require("request");
-const movieID = process.argv[2] || "";
+const req = require('request');
+const movieID = process.argv[2] || '';
 const endpoint = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
 req.get(endpoint, (err, response, body) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	const obj = JSON.parse(body);
-	console.log(obj?.title);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const obj = JSON.parse(body);
+  console.log(obj?.title);
 });
