d0d5b2d8dcaaf5c207b84c88323a43d172bc994a
lil-armstrong <oebong1@gmail.com>
2023-05-24 22:43:24 +0100

    refactor

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
-const req = require("request");
+const req = require('request');
 
 const url = process.argv[2];
 
 req.get(url, (err, response) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	if (response.statusCode === 200) {
-		const obj = JSON.parse(response?.body);
-		const completed = {};
+  if (err) {
+    console.error(err);
+    return;
+  }
+  if (response.statusCode === 200) {
+    const obj = JSON.parse(response?.body);
+    const completed = {};
 
-		obj.forEach((task) => {
-			if (task.completed) {
-				if (task.userId in completed) {
-					completed[task.userId]++;
-				} else {
-					completed[task.userId] = 1;
-				}
-			}
-		});
+    obj.forEach((task) => {
+      if (task.completed) {
+        if (task.userId in completed) {
+          completed[task.userId]++;
+        } else {
+          completed[task.userId] = 1;
+        }
+      }
+    });
 
-		console.log(completed);
-		return;
-	}
+    console.log(completed);
+    return;
+  }
 
-	console.error(`Error code: ${response.statusCode}`);
+  console.error(`Error code: ${response.statusCode}`);
 });
